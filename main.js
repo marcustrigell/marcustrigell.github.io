9724227afa88baea3618ba870e066b3fa056ecf3
marcustrigell <marcustrigell@gmail.com>
2022-02-05 18:33:01 +0100

    working scrollable 3d portfolio stuff

--- main.js
+++ main.js
@@ -17,25 +17,30 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
+// Torus
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const material = new THREE.MeshStandardMaterial({color: 0x7209B7});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+// Ligthing
 const pointLight = new THREE.PointLight(0xFFFFFF);
 pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
 
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
+// Helpers
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
 
-scene.add(lightHelper, gridHelper);
+// scene.add(lightHelper, gridHelper);
 
+// Mouse controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Stars
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
@@ -49,6 +54,55 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
+// Background
+const spaceTexture = new THREE.TextureLoader().load('octavian-rosca-UfCYo7zHyY8-unsplash.jpg');
+scene.background = spaceTexture;
+
+// Avatar
+const marcusTexture = new THREE.TextureLoader().load('marcus.png');
+
+const marcus = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({map: marcusTexture})
+);
+
+scene.add(marcus);
+
+// Moon
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+
+// Move camera scroll
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  marcus.rotation.y += 0.01;
+  marcus.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+
+// Main loop
 function animate() {
   requestAnimationFrame(animate);
 
